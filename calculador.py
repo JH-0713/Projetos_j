a7060040d55b0bf5ca5532ef56009f780d3bd418
senai_25_26 <jhmsg45@gmail.com>
2026-02-09 09:44:03 -0300

    Initial commit

---
+++ calculador.py
@@ -0,0 +1,28 @@
+print('-' * 26)
+print('Bem vindo ao Calculadora J')
+print('-' * 26)
+print('')
+print('')
+print('')
+print('')
+print('')
+'''
+print('Clique Tecla [ENTER] para continuar:')
+print('')
+input('> ')
+'''
+print('')
+print('')
+print('')
+list_numero_1 = []
+list_numero_save = []
+list_result = []
+while True:
+    print('  ⌈‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾⌉   ')
+    print('  |⌈                                  ⌉|   ')
+    print(f'  ||                {list_numero_save}            ||   ')
+    print('  ||                                  ||   ')
+    print('  ||                                  ||   ')
+    print('  |⌊                                  ⌋|   ')
+    print('  ⨽—–-−¯_–‾⌟⌞−⨼   ')
+    break
